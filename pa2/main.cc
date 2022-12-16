739d312fcfc85306b7a3e2259f01de428b2761b5
y2792li <y2792li@uwaterloo.ca>
2022-12-16 01:14:56 -0500

    add 3 TseitinTransformer add functions

--- pa2/main.cc
+++ pa2/main.cc
@@ -1,12 +1,102 @@
 #include "tseitinTransformer.h"
 #include "satSolver.h"
+#include <iostream>
+#include <string>
+#include <map>
+#include "parser.h"
+#include "treeNode.h"
+
+std::string falseType[] = { "invalid input", "incomplete assignment", "contradicting assignment"};
+std::vector<int> arr =  {0,0,0};
+int code = -1;
+TreeNode* root;
+std::map<std::string, bool> assignment;
+
+// a helper function parsing the input into the formula string and the assignment string
+void parseLine(const std::string &line, std::string &formulaStr, std::string &assignmentStr) {
+  // your code starts here
+  FormulaParser* fp = new FormulaParser(formulaStr);
+  root = fp->getTreeRoot();
+
+  if (fp->hasToken()) {
+    code = 0;
+    arr[code] = 1;
+    // std::cout << "Error: " << falseType[code] << std::endl;
+  }
+  else if (fp->getFalse() != -1) {
+    code = fp->getFalse();
+    arr[code] = 1;
+    // std::cout << "Error: " << falseType[code] << std::endl;
+  }
+
+  AssignmentParser* ap = new AssignmentParser(assignmentStr);
+  assignment = ap->parseAssignment();
+  if (ap->getFalse() != -1) {
+    code = ap->getFalse();
+    arr[code] = 1;
+  }
+}
 
 // The program shall continuously ask for new inputs from standard input and output to the standard output
 // The program should terminate gracefully (and quietly) once it sees EOF
 int main() {
-  while (true) // continuously asking for new inputs from standard input
+ std::string line; // store each input line
+  while (std::getline(std::cin, line)) // continuously asking for new inputs from standard input
   {
-    std::string line; // store each input line
+    std::string formulaStr; // store the formula string
+    std::string assignmentStr; // store the assignment string
     // your code starts here
+    int len = line.size();
+    int split;
+    for (split = 0; split < len && line[split] != ';'; split++) {
+      // formulaStr += line[split];
+    }
+    formulaStr = line.substr(0, split);
+    if (split + 1 < len) {
+      assignmentStr = line.substr(split + 1, len - 1 - split);
+    }
+    else if (line[split] != ';'){
+      code = 0;
+      arr[code] = 1;
+    }
+    parseLine(line, formulaStr, assignmentStr);
+
+    Tokenizer t(formulaStr);
+    std::vector<std::string> var;
+    for (int i = 0; i < t.getTotal(); i++) {
+      if (t.getToken().type == "VAR_NAME") {
+        var.push_back(t.getToken().content);
+      }
+      t.addCounter();
+    }
+    for (size_t i = 0; i < var.size(); i++) {
+      if (!assignment.count(var[i])) {
+        code = 1;
+        arr[code] = 1;
+      }
+    }
+    if (code == -1) {
+      bool res = root->evaluate(assignment);
+      std::cout << res << std::endl;
+    }
+    else {
+      if (arr[0] == 1) {
+        std::cout << "Error: " << falseType[0] << std::endl;
+      }
+      else if (arr[1] == 1) {
+        std::cout << "Error: " << falseType[1] << std::endl;
+      }
+      else if (arr[2] == 1) {
+        std::cout << "Error: " << falseType[2] << std::endl;
+      }
+      else {
+        std::cout << "Wrong with code" << std::endl;
+      }
+    }
+    code = -1;
+    arr[0] = 0;
+    arr[1] = 0;
+    arr[2] = 0;
   }
+  return 0;
 }
--- pa2/tseitinTransformer.cc
+++ pa2/tseitinTransformer.cc
@@ -5,16 +5,20 @@ TseitinTransformer::TseitinTransformer(TreeNode *root): formulaRoot{root} {}
 int TseitinTransformer::transSubformula(TreeNode* subRoot) {
   // your code starts here
   std::string content = subRoot->getContent();
+  int currIdCounter = varIdCounter;
   if (content == "-") {
+    varIdCounter++;
     int child = transSubformula(subRoot->getLeftChild());
     addNegEq(varIdCounter, child);
   }
   else if (content == "+") {
+    varIdCounter++;
     int l_child = transSubformula(subRoot->getLeftChild());
     int r_child = transSubformula(subRoot->getRightChild());
     addOrEq(varIdCounter, l_child, r_child);
   }
   else if (content == "*") {
+    varIdCounter++;
     int l_child = transSubformula(subRoot->getLeftChild());
     int r_child = transSubformula(subRoot->getRightChild());
     addAndEq(varIdCounter, l_child, r_child);
@@ -23,29 +27,39 @@ int TseitinTransformer::transSubformula(TreeNode* subRoot) {
     if (varIdTable.count(content)) {
       return varIdTable[content];
     }
-    else {
+    else {  // new variable to be assigned
       varIdTable[content] = varIdCounter;
+      varIdCounter++;
     }
   }
-  varIdCounter++;
-  return varIdCounter;
+  return currIdCounter;
 }
 
 void TseitinTransformer::addNegEq(int curID, int childID) {
   // your code starts here
+  // std::vector<int> arr = [{-curID, -childID}, {curID, childID}];
+  cnf.push_back({-curID, -childID});
+  cnf.push_back({curID, childID});
 }
 
 void TseitinTransformer::addOrEq(int curID, int leftID, int rightID) {
   // your code starts here
+  cnf.push_back({-curID, leftID, rightID});
+  cnf.push_back({curID, -leftID});
+  cnf.push_back({curID, -rightID});
 }
 
 void TseitinTransformer::addAndEq(int curID, int leftID, int rightID) {
   // your code starts here
+  cnf.push_back({curID, -leftID, -rightID});
+  cnf.push_back({-curID, leftID});
+  cnf.push_back({-curID, rightID});
 }
 
 std::vector<std::vector<int>> TseitinTransformer::transform() {
   // your code starts here
-  
+  transSubformula(formulaRoot);
+  cnf.push_back({1});
   return cnf;
 }
 
